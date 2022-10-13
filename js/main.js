f27ae3915ff43d97d3eb00d02620515a97a5384a
Bozeyy <spereaux2@gmail.com>
2022-10-13 14:46:27 +0200

    adaptation du jeu sur la page

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
-const world = document.getElementById('game');
+const world = document.querySelector('#game');
 const c = world.getContext('2d');
-const tailleRef = world.width / 50;
 
+var btn = document.querySelector(".btn-play");
+
+world.width = window.innerHeight;
+world.height = window.innerHeight;
+const tailleRef = world.width/25;
 
 const imgVesseau = new Image();
-imgVesseau.src = 'img/Vesseau.png';
+imgVesseau.src = 'img/ship.png';
+
+
+let Continue = true;
+
+let Pause = true;
 
 class Player {
     constructor() {
@@ -16,12 +25,13 @@ class Player {
         }
         this.position = {
             x: (world.width - this.width) / 2,
-            y: world.height - this.height 
+            y: world.height - this.height*1.3
         }
     }
 
     draw() {
         c.drawImage(imgVesseau, this.position.x, this.position.y, this.width, this.height);
+        //c.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
 
     update() {
@@ -37,24 +47,57 @@ const player = new Player();
 
 let frames = 0;
 
-AnimationLoop();
 
 function AnimationLoop() {
     requestAnimationFrame(AnimationLoop);
     c.clearRect(0, 0, world.width, world.height);
-    player.update();
+    if (Continue) {
+
+        if (Pause) {
+            // rendre le world invisible
+            world.setAttribute("display", "none");
+            console.log('pause');
+        } else {
+            player.update();
+        }
+
+
+
+    }
     frames++;
 }
+
+
+// methode jouer
+function Play() {
+    c.clearRect(0, 0, world.width, world.height);
+    console.log('play');
+    btn.parentNode.removeChild(btn);
+    Pause = false;
+    AnimationLoop();
+}  
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // EventListeners
 document.addEventListener('keydown', (event) => {
     switch (event.key) {
         case 'ArrowLeft':
-            player.velocity.x = -2;
+            player.velocity.x = -tailleRef/6;
             break;
         case 'ArrowRight':
-            player.velocity.x = 2;
+            player.velocity.x = tailleRef/6;
             break;
     }
 });
@@ -66,6 +109,9 @@ document.addEventListener('keyup', (event) => {
             break;
         case 'ArrowRight':
             player.velocity.x = 0;
+            break;
+        case 'Escape':
+            Pause();
             break;
     }
 });
